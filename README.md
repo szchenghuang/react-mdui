6ffffdb73d2b515b394207b4bb7409bb7efaa884
szchenghuang <szchenghuang@gmail.com>
2016-12-30 16:10:59 +0800

    Initialization.

---
+++ README.md
@@ -0,0 +1,15 @@
+[![NPM Package](https://img.shields.io/npm/v/react-mdui.svg)](https://www.npmjs.org/package/react-mdui)
+[![Dependency status](https://david-dm.org/szchenghuang/react-mdui/status.svg)](https://david-dm.org/szchenghuang/react-mdui/)
+[![devDependency status](https://david-dm.org/szchenghuang/react-mdui/dev-status.svg)](https://david-dm.org/szchenghuang/react-mdui/?type=dev)
+
+# react-mdui #
+
+[MDUI](https://github.com/zdhxiong/mdui) in ReactJS.
+
+## Install ##
+
+## Usage ##
+
+## License ##
+
+MIT. See [LICENSE.md](http://github.com/szchenghuang/react-mdui/blob/master/LICENSE.md) for details.
---
+++ dist/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//#############################################################################
+// Library includes.
+//#############################################################################
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Slider = undefined;
+
+require('mdui/dist/css/mdui.min.css');
+
+require('mdui/dist/js/mdui.min.js');
+
+var _slider = require('./slider');
+
+var _slider2 = _interopRequireDefault(_slider);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+//#############################################################################
+// Exports.
+//#############################################################################
+exports.Slider = _slider2.default;
+
+//#############################################################################
+// Application includes.
+//#############################################################################
